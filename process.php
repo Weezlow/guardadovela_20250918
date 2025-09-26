9d6853f02ef5dd7bc58a4181969339ca04bc6ba8
Weezlow <alexisguardadovela@gmail.com>
2025-09-25 19:52:23 -0600

    Practica finalizada

--- process.php
+++ process.php
@@ -1,11 +1,28 @@
 <?php
-
-require("classes/estudiante.Class.php");
+require("classes/estudiante.class.php");
 $Estudiante = new Estudiante();
 
-$resultado = $Estudiante->obtenerEstudiantes();
+$resultado = [];
 
-header("Content-Type: Application/json");
+if($_SERVER["REQUEST_METHOD"]=="GET"){
+    $tipo_peticion = ( (issue($_GET["t"])) ? ($_GET["t"]) : null );
+
+    if($tipo_peticion=="selectAll"){
+        $resultado = $Estudiante->obtenerEstudiante();
+    }else{
+        $resultado = [];
+    }
+    
+}elseif($_SERVER["REQUEST_METHOD"]=="POST"){
+    if(isset($_POST["fnac"]) and isset($_POST["idg"])){
+        $resultado = $Estudiante->nuevoEstudiante($_POST["fnac"],$_POST["idg"]);
+    }else{
+        header('HTTO/1.1 400 Bad Request');
+        $resultado = array("mensaje"=>"Parametros no enviados");
+    }
+}
+
+header("Content-Type:Application/json");
 echo(json_encode($resultado));
 
 ?>
